382560c1843b9258f9ddd7a1d647c695c677c709
Jonathan Böcker <jonathan.bocker@smartrefill.se>
2019-06-23 17:18:49 +0200

    Updates

---
+++ precache-manifest.5a5dcd81fbcb70014fecaf64820241f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47782b1d9ef6aaac18d564abbaa2093e",
+    "url": "/roadwork/index.html"
+  },
+  {
+    "revision": "9ea5744e4712c0fbba96",
+    "url": "/roadwork/static/css/main.855a93b2.chunk.css"
+  },
+  {
+    "revision": "d485f027d51c0cc885c5",
+    "url": "/roadwork/static/js/2.9aa6d76e.chunk.js"
+  },
+  {
+    "revision": "9ea5744e4712c0fbba96",
+    "url": "/roadwork/static/js/main.42688bbb.chunk.js"
+  },
+  {
+    "revision": "82e517c2dbc1356dab0c",
+    "url": "/roadwork/static/js/runtime~main.2401f7f8.js"
+  }
+]);
